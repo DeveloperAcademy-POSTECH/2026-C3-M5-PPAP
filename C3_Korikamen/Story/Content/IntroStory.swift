dadf075788ae4fd77ee0f3ef535b3af23fca7510
leehwanhun <127852445+nansis4457@users.noreply.github.com>
2026-06-06 15:02:18 +0900

    refactor(story): 스토리별 애셋 관리를 위해 content 폴더 추가

---
+++ C3_Korikamen/Story/Content/IntroStory.swift
@@ -0,0 +1,104 @@
+//
+//  IntroStory.swift
+//  C3_Korikamen
+//
+//  Created by 이환훈 on 6/5/26.
+//
+
+import Foundation
+import SwiftUI
+
+
+let introStory: [StoryPage] = [
+    //컷 1
+    StoryPage(
+        background: "bg_prelude1", //일단 검은색으로
+        panel: StoryPanel(text: "1922년 Apple Developer Academy @ Egypt..")
+        ),
+    
+    // 컷 2
+        StoryPage(
+            background: "bg_prelude2",
+            panel: StoryPanel(text: "세상은 아직 몰랐다. 전설의 유물, 코리카멘의 무덤이 존재한다는 사실을..")
+        ),
+    // 컷 3
+    StoryPage(
+        background: "bg_prelude3",
+        panel: StoryPanel(text: "그.. 요즘 루미 멘토 조금 이상하지 않아요?")
+    ),
+    // 컷 4 -> 루미 멘토 추가해야함
+    StoryPage(
+        background: "bg_prelude4",
+        panel: StoryPanel(text: "그니까요! 막 금목걸이에 금애플워치까지...")
+    ),
+    // 컷 5 -> 루미 멘토 추가해야함
+    StoryPage(
+        background: "bg_prelude5",
+        panel: StoryPanel(text: "저 어제 봤어요 퇴근할 때 삽들고 나가는 거. **이건 100% 도굴이에요.** ")
+    ),
+    // 컷 6
+    StoryPage( //-> notice 추가해야함
+        background: "bg_prelude6"),
+    
+    // 컷 7
+    StoryPage(
+        background: "bg_prelude7",
+        panel: StoryPanel(text: "보자보자..내 통장잔고....")
+    ),
+    // 컷 8
+    StoryPage(
+        background: "bg_prelude8",
+        panel: StoryPanel(text: "143원...?!??!! 학습지원금 110만원은 어디로...")
+    ),
+    // 컷 9 -> 루미 멘토 추가해야함
+    StoryPage(
+        background: "bg_prelude9",
+        panel: StoryPanel(text: "**코리카멘 전설의 황금 유물.. 찾기만 하면 평생 놀고 먹고 살 수 있을텐데..**")
+    ),
+    // 컷 10
+    StoryPage(
+        background: "bg_prelude10",
+        ),
+    // 컷 11
+    StoryPage(
+        background: "bg_prelude11",
+        ),
+    
+    // 컷 12 -> 루미 멘토 추가해야함
+    StoryPage(
+        background: "bg_prelude12",
+        panel: StoryPanel(text: "좋아.. **루미보다 먼저 간다**")
+    ),
+    // 컷 13
+    StoryPage(
+        background: "bg_prelude13"),
+    
+    // 컷 14
+    StoryPage(
+        background: "bg_prelude14",
+        panel: StoryPanel(text: "이제 들어간다..")
+    ),
+    // 컷 15 (전환 씬)
+    StoryPage(
+        background: "bg_prelude15", transition: .fade(.black,1.0)
+    ),
+    // 컷 16
+    StoryPage(
+        background: "bg_prelude16",
+        panel: StoryPanel(text: "저건 뭐지..?")
+    ),
+    // 컷 17
+    StoryPage(
+        background: "bg_prelude17",
+        panel: StoryPanel(text: "왕의 보호막을 깨는 자, 엄청난 부를 얻게 되리라...?")
+    ),
+    // 컷 18 (전환 씬)
+    StoryPage(
+        background: "bg_prelude18", transition: .fade(.white,1.0)
+    ),
+    // 컷 19 (전환 씬)
+    StoryPage(
+        background: "bg_prelude19", transition: .fade(.white,1.0)
+    )
+
+]
